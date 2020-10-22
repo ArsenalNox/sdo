029ccffcf7dde527bfbad07ac765d65099e5c0e4
ArsenalNox <nohtimore@gmail.com>
2020-10-22 09:41:30 +0500

    а

--- create_module.php
+++ create_module.php
@@ -29,11 +29,28 @@
       <hr>
       <h2> Создание модуля </h2>
       <form class='new_module_form' action="submit_new_module.php" method="post" id='nmf'>
-        <textarea id='mn' name="module_name" rows="2" cols="40" style="resize:none" placeholder="Название модуля"></textarea>
-        <textarea id='ms' name="module_subject" rows='2' cols='40' style="resize:none" placeholder="Премет модуля"></textarea>
+        <textarea id='mn' name="module_name" rows="2" cols="40" style ="resize:none" placeholder="Название модуля"></textarea>
+        <textarea id='ms' name="module_subject" rows='2' cols='40' style ="resize:none" placeholder="Премет модуля"></textarea>
       </form>
       <hr>
       <button type="button" name="button" onclick='addQuestion()'> Добавить вопрос </button>
     </section>
+  <script type="text/javascript">
+    var current_question_number = 1;
+
+    function addQuestion(){
+      current_question_number++
+      //Создание оболочки для текста вопроса
+      var new_question = document.createElement('div');
+      new_question.className = 'question';
+      new_question.id = "q_"+current_question_number;
+      var question_textarea = document.createElement('textarea');
+      question_textarea.name = 'question_text_'+current_question_number;
+      question_textarea.rows = '8';
+      question_textarea.cols = '80';
+      question_textarea.style.resize = "none";
+    }
+
+  </script>
   </body>
 </html>
