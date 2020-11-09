36bd1721780bd5df24fa62db276e8b529623beb2
dimrio323 <62644864+dimrio323@users.noreply.github.com>
2020-11-09 14:41:28 +0500

    css

--- panel-login.php
+++ panel-login.php
@@ -43,8 +43,8 @@
               <legend align="center"><h1 class="text">ВХОД</h1></legend>
               <input type="text" name="uid" placeholder="Логин" class="loginIn">
               <input type="password" name="pwd" placeholder="Пароль" class="password">
-              <?php if(isset($error)){echo"<p> $error </p>";} ?>
-              <button type="submit" name="" value="Войти" class="button">Войти</button>
+                
+              <button type="submit" name="" value="Войти" class="button" id="entrance">Войти</button>
             </fieldset>
           </form>
         </div>
