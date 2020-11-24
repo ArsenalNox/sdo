d460d36cad38db8947c445d603bca6e64b4b8f51
ArsenalNox <nohtimore@gmail.com>
2020-11-24 16:40:22 +0500

    ~40% импорта (пока не импортирует)

--- import.php
+++ import.php
@@ -3,6 +3,7 @@
 // TODO: Превью загруженной таблицы, конпка подверждения экспорта
 // TODO: Экспорт в submit module
 
+
 include_once "dtb/dtb.php";
 require 'php/vendor/autoload.php';
 
@@ -48,11 +49,56 @@ if(isset($_COOKIE['STS'])){
               $reader = IOFactory::createReader('Xlsx');
               $spreadsheet = $reader->load($_FILES['uploadedFile']['tmp_name']);
               $writer = IOFactory::createWriter($spreadsheet, 'Html');
-              $message = $writer->save('php://output');
+              // $message = $writer->save('php://output');
             }
+            echo"<hr> <form>
+            <input type='text' name='Module_name' placeholder='Название модуля'>
+            <input type='text' name='module_subject' placeholder='Название модуля'>
+            ";
+            while(true){
+              $qquant = 0;
+              for ($i=6; $i<8 ; $i++) {
+                $variant = 1;
+                for ($j=8; $j<13 ; $j++) {
+                  $qnum = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(2, $i)->getValue();
+                  $qtype = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(3, $i)->getValue();
+                  $qsubtype = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(4, $i)->getValue();
+                  $qcomm = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(5, $i)->getValue();
+                  $qtextF = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(6, $i)->getValue();
+                  $qtextL = $spreadsheet->getActiveSheet()->getCellByColumnAndRow($j, $i)->getValue();
+                  $qansw = $spreadsheet->getActiveSheet()->getCellByColumnAndRow($j+1, $i)->getValue();
+                  echo "
+                  <div>
+                  <p> Номер вопроса: $qnum, Вариант: $variant</p>
+                  <p> Вид задания: $qtype </p>
+                  <p> Подвид задания: $qsubtype </p>
+                  <p> Комментарий к заданию: $qcomm </p>
+                  <p> Текст вопроса: $qtextF $qtextL </p>
+                  <p> Правильный ответ: $qansw </p>
+                  <hr>
+                  <fieldset>
+                    <input type='hidden' name='QUESTION_NUM'>
+                    <input type='hidden' name=''>
+                    <input type='hidden' name=''>
+                    <input type='hidden' name=''>
+                    <input type='hidden' name=''>
+                    <input type='hidden' name=''>
+                  </fieldset>
+                  </div>
+                  ";
+                  $variant++;
+                  $qquant++;
+                }
+              }
+              break;
+            }
+            echo"
+
+            <button> Загрузить данный тест </button>  </form> $qquant ";
+
           }
         }
-        echo "$message";
          ?>
+      </section>
   </body>
 </html>
