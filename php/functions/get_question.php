ce4c52f9641d89d734fb9714cedc23bc80f9880f
ArsenalNox <nohtimore@gmail.com>
2020-12-11 15:55:04 +0500

    Изменил способ отображения вопросов преподавателю

--- php/functions/get_question.php
+++ php/functions/get_question.php
@@ -1,5 +1,7 @@
 <?php
 //Показ всех вопросов выбранного модуля
+// TODO: Функция выбора показа конкретного варианта
+
 include_once "../../dtb/dtb.php";
 if($_SERVER['REQUEST_METHOD'] == "POST"){
   $module = $_POST['module_name'];
@@ -7,10 +9,13 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
   $result = mysqli_query($conn, $sql);
   $data = mysqli_fetch_assoc($result);
   $path = $data['Questions'];
-  $string = file_get_contents("../../$path");
-  $json_a = json_decode($string, true);
-  $i = 0;
-  $showmeta = true;
+  if(file_get_contents("../../$path")){
+    $string = file_get_contents("../../$path");
+  } else {
+    echo "<h3> Ошибка: Не получилось открыть файл модуля </h3>";
+    die();
+  }
+
   // echo "<select onselect='ShowSpecificVariants()' id='varsel'>
   // <option value='0'> Показать все варианты       </option>
   // <option value='1'> Показывать только вариант 1 </option>
@@ -18,76 +23,55 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
   // <option value='3'> Показывать только вариант 3 </option>
   // <option value='4'> Показывать только вариант 4 </option>
   // <option value='5'> Показывать только вариант 5 </option>
-  // </select>
-  // " ;
+  // </select>" ;
+
+  //$i - кол-во итераций в JSON'е модуля
+  $i = 0;
+  //Флаг показа мета информации
+  $showmeta = true;
+
+  $json_a = json_decode($string, true);
   foreach ($json_a as $struct => $quest) {
     if($showmeta){
+      //Показываем мету
       $showmeta=false;
-      $quest_quantity = $quest['quest_quantity'];
-      echo "
-      <p> Название модуля: ". $quest['Module_name'] ." </p>
-      <p> Кол-во вопросов: ".$quest['quest_quantity']."</p>
-      ";
+      if(isset($quest['Module_name'])){
+        echo "<p> Название модуля: ". $quest['Module_name'] ." </p>"; }
+      if(isset($quest['quest_quantity'])){
+        $quest_quantity = $quest['quest_quantity'];
+        echo "<p> Кол-во вопросов: ".$quest['quest_quantity']."</p>"; }
       continue;
     }
+
     $i++;
     if($quest['IMAGE'] == ''){
       $image = '';
     } else {
       $image = "<img src='/sdo".$quest['IMAGE']."'>";
     }
-    switch ($quest['NUM_ANSW']) {
-      case '2':
-        echo
-          "<div class='task' id='n" . $quest['QUESTION_NUM'] . "-v" . $quest['VAR'] . "-" . $quest['QUESTION_NUM'] . "'>
-          <h4> Задание №" . $quest['QUESTION_NUM'] . "
-          Вариант " . $quest['VAR'] . " $image
-          </h4> " . $quest['QUESTION'] . " <br>
-          A) " . $quest['A'] . " ;
-          B) " . $quest['B'] . " ;
-          <br> <b> Правильный ответ: " . $quest['CORRECT'] . " </b>
-          <hr> </div>";
-        break;
-      case '3':
-        echo
-          "<div class='task' id='n" . $quest['QUESTION_NUM'] . "-v" . $quest['VAR'] . "'>
-          <h4> Задание №" . $quest['QUESTION_NUM'] . "
-          Вариант " . $quest['VAR'] . " $image
-          </h4> " . $quest['QUESTION'] . " <br>
-          A) " . $quest['A'] . " ;
-          B) " . $quest['B'] . " ;
-          C) " . $quest['C'] . " ;
-          <br> <b> Правильный ответ: " . $quest['CORRECT'] . " </b>
-          <hr> </div>";
-        break;
-      case '4':
-        echo
-          "<div class='task' id='n" . $quest['QUESTION_NUM'] . "-v" . $quest['VAR'] . "'>
-          <h4> Задание №" . $quest['QUESTION_NUM'] . "
-          Вариант " . $quest['VAR'] . " $image
-          </h4> " . $quest['QUESTION'] . " <br>
-          A) " . $quest['A'] . " ;
-          B) " . $quest['B'] . " ;
-          C) " . $quest['C'] . " ;
-          D) " . $quest['D'] . "
-          <br> <b> Правильный ответ: " . $quest['CORRECT'] . " </b>
-          <hr> </div>";
-        break;
+
+    echo "<div class='task' id='n" . $quest['QUESTION_NUM'] . "-v" . $quest['VAR'] . "-" . $quest['QUESTION_NUM'] . "'>
+    <h4> Задание №" . $quest['QUESTION_NUM'] . "
+    Вариант " . $quest['VAR'] . " $image
+    </h4> " . $quest['QUESTION'] . " <br>";
+    if(isset($quest['A'])){
+      echo "  A) " . $quest['A'] . " ;";
     }
+    if(isset($quest['A'])){
+      echo "  A) " . $quest['A'] . " ;";
+    }
+    if(isset($quest['A'])){
+      echo "  A) " . $quest['A'] . " ;";
+    }
+    if(isset($quest['A'])){
+      echo "  A) " . $quest['A'] . " ;";
+    }
+    echo "<br> <b> Правильный ответ: " . $quest['CORRECT'] . " </b>
+    <hr> </div>";
   }
-  // for ($j=1; $j < $quest_quantity+1; $j++) {
-  //     echo '
-  //     <p> Задание '. $j .' </p>
-  //    <input type="radio" id="'.$j.'" name="testn'.$j.'" value="1">
-  //    <label for="male">ВКЛ</label>
-  //    <input type="radio" id="'.$j.'" name="testn'.$j.'" value="0">
-  //    <label for="female">ВЫКЛ</label>
-  //    <br>
-  //    ';
-  // }
   echo "
     <p> Всего вопросов: $quest_quantity, не учитывая варианты, <span id='qnum'>$i</span> учитывая</p>
-    Время на выполнение в минутах <input type='number' name='time' placeholder='45' style='width:40px;' id='test_time'>
+    Время на выполнение в минутах <input type='number' name='time' placeholder='45' value='45' style='width:40px;' id='test_time'>
     <br>
     <button class='nexttest' type='button' name='button' onclick='StartTest()' >Начать тест</button>
     ";
