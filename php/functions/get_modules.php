be36c4e7590d6eff42af070e115acdbf136a7ac5
ArsenalNox <nohtimore@gmail.com>
2020-12-12 13:08:59 +0500

    Быстрофикс

--- php/functions/get_modules.php
+++ php/functions/get_modules.php
@@ -2,7 +2,11 @@
 //Получение модуля по классу
 include_once "../../dtb/dtb.php";
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-	$groupNumber = preg_replace('/[^0-9]/',' ',$_POST['group']);
+		if(isset($_POST['group'])){
+			$groupNumber = preg_replace('/[^0-9]/',' ',$_POST['group']);
+		}else {
+			$groupNumber = '';
+		}
   	$subj = $_POST['subject'];
   	$sql = "SELECT Name FROM new_module WHERE subject='$subj' AND Class = '$groupNumber'";
   	$result = mysqli_query($conn, $sql);
