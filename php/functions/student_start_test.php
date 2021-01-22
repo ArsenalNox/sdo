c144e8ec7d511a823df45ed25c4bda90ffc098a9
dimrio323 <62644864+dimrio323@users.noreply.github.com>
2021-01-22 14:56:13 +0500

    шрифт

--- php/functions/student_start_test.php
+++ php/functions/student_start_test.php
@@ -79,7 +79,7 @@ foreach ($json_a as $struct => $quest) {
           } else {
             $_SESSION["QUESTION_IMAGE_$qselector"] = '';
           }
-		echo "<p style='color: #606060;' class='font'>" .  $quest['QUESTION'] . " </p> <br>";
+		echo "<p class='font'>" .  $quest['QUESTION'] . " </p> <br>";
 		  if(isset($quest['A'])){
 			echo "<input type='radio' id='A$qselector' name='ANSW_$qselector' value='".$quest['A']."' required> <label for='A$qselector'> A) ".$quest['A']." </label> <br>"; 
 		  }		  
