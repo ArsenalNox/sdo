0575238839ef5c2a3a067ec243b6a0ac5326f23d
Komenda999 <40666573+Komenda999@users.noreply.github.com>
2021-02-02 15:19:28 +0300

    Дополнение БД
    - доп. функции для работы с БД
    - балванка admin.php

--- admin.php
+++ admin.php
@@ -1,219 +1,204 @@
 <?php
-require_once 'dtb/dtb.php';
-$groups = [];
-if ($result = mysqli_query($conn, "SELECT FROM group_student", MYSQLI_USE_RESULT)) {
-
-    /* Важно заметить, что мы не можем вызывать функции, которые взаимодействуют
-       с сервером, пока не закроем результирующий набор. Все подобные вызовы
-       будут вызывать ошибку 'out of sync' */
-    if (!mysqli_query($link, "SET @a:='this will not work'")) {
-        printf("Ошибка: %s\n", mysqli_error($link));
-    }
-    mysqli_free_result($result);
-}
-if(isset($_POST['users'])){
-    foreach($_POST['users'] as $user)
-    {
-        $sql = "INSERT INTO `student`(`NAME`, `LAST_NAME`, `MIDDLE_NAME`, `GROUP_STUDENT_ID`) VALUES ({$user['name']}, {$user['last']}, {$user['middle']}, {$user['group_id']})";
-        mysqli_query($conn, $sql);
-    }
-}
-
+require_once 'dtb/dtb_funcs.php';
+$groups = dbquery('SELECT * FROM group_student');
 ?>
 
 <form action="">
+    <select name="GROUP_STUDENT_ID">
+        <option value="null" selected disabled>Выберите класс</option>
+        <?php foreach($groups as $group): ?>
+          <option value="<?=$group['ID']?>"><?=$group['NAME']?></option>
+        <?php endforeach; ?>
+    </select>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        <select name="GROUP_STUDENT_ID" id="">
-            <option value="">Выберите класс</option>
-        </select>
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
     <div class="item">
         <input name="NAME" type="text" placeholder="Введите Имя">
         <input name="LAST_NAME" type="text" placeholder="Введите Фамилию">
         <input name="MIDDLE_NAME" type="text" placeholder="Введите Отчество">
-        
+
     </div>
 </form>
---
+++ dtb/dtb_funcs.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "dtb.php";
+
+function dbquery($sql)
+{
+  global $conn;
+  $result = mysqli_query($conn, $sql);
+  $res = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  mysqli_free_result($result);
+  mysqli_close($conn);
+  return $res;
+}
+
+function dbexecute($sql)
+{
+  global $conn;
+  return mysqli_query($conn, $sql);
+}
