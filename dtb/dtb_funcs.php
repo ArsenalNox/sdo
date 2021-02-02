f5d6d7fd0e2e7e3ee15cd70d6bad0ee97b619648
Pion4r <staryliss.nikita.2004@gmail.com>
2021-02-02 22:53:40 +0500

    Admin#2

--- dtb/dtb_funcs.php
+++ dtb/dtb_funcs.php
@@ -8,15 +8,15 @@ function dbquery($sql)
   $result = mysqli_query($conn, $sql);
   $res = mysqli_fetch_all($result, MYSQLI_ASSOC);
   mysqli_free_result($result);
-  mysqli_close($conn);
   return $res;
 }
 
 function dbexecute($sql)
 {
   global $conn;
-  if(mysqli_query($conn, $sql))
+  if($result = mysqli_query($conn, $sql))
   {
+    mysqli_free_result($result);
     return true;
   }
   return false;
