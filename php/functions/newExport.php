5389defe283b8797e7b5b760d2841ab397408f31
ArsenalNox <nohtimore@gmail.com>
2021-03-24 22:56:27 +0500

    Проверка на соединение с бд, пробелы/комментарии

--- php/functions/newExport.php
+++ php/functions/newExport.php
@@ -1,34 +1,35 @@
 <?php
 session_start();
 
-$user = 'root';
-$password = '';
-$server = 'localhost';
-$database = 'sdo3';
+require_once '../../dtb/dtb.php';
+if(isset($user) & isset($password) & isset($server) & isset($database)){
+    $dtb = new PDO("mysql:host=$server;dbname=$database;charset=utf8", $user, $password);
+} else {
+    $user = 'root';
+    $password = '';
+    $server = 'localhost';
+    $database = 'sdo';
 
-$dtb = new PDO("mysql:host=$server;dbname=$database;charset=utf8", $user, $password);
+    $dtb = new PDO("mysql:host=$server;dbname=$database;charset=utf8", $user, $password);
+}
 
 require '../vendor/autoload.php';
 
-use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Style\Color;
 use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Style\Conditional;
-use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
-use PhpOffice\PhpSpreadsheet\Writer\Ods\Content;
 
-//Никаких проверок пока что не будет, просто выплёвывает xlsx файл
+//TODO: Опции выборки 
 
 $sql = 'SELECT * FROM test_results';
 $stm = $dtb->query($sql);
 if($stm){
-    //Указатели на столбец/строку
+    //Указатели на столбец/строку (Точнее сколько ячеек было пройденно в ряду/колонне, да названия перепутались но если подумать не так страшно)
     $s_column = 1; 
     $s_row = 1;
 
-    //Указаиелт на номера колонок, котрые нужно скрыть
     //Сама таблица 
     $spreadSheet = new Spreadsheet();
     $sheet = $spreadSheet->getActiveSheet(); //Рабочий лист 
@@ -56,17 +57,21 @@ if($stm){
     $all_conditional_styles[] = $condition_correctness_false;
 
     $hideUnnecessaryColumns = true;
+
     while($row = $stm->fetch(PDO::FETCH_ASSOC)){ //Итерация через все результаты 
-        $sheet->setCellValueByColumnAndRow(1, $s_column, $row['id']);
+        //Записывам id/фио
+        $sheet->setCellValueByColumnAndRow(1, $s_column, $row['id']); 
         $sheet->setCellValueByColumnAndRow(2, $s_column, $row['student']);
         $sql = "SELECT * FROM tr_".$row['id']."";
+        
         $s_row = 3;
+
         $question_count = 0; 
         $wrong_answers = 0;
+        
         if($dtb->query($sql)){
             $result = $dtb->query($sql);
-            while($tr_row = $result->fetch(PDO::FETCH_ASSOC)){
-            //foreach($dtb->query($sql) as $tr_row){
+            while($tr_row = $result->fetch(PDO::FETCH_ASSOC)){ //Итерация по вопросам конкретного теста 
                 //Записывание название колонны
                 $question_count++;
                 $sheet->setCellValueByColumnAndRow($s_row,   1, 'Задание '.$tr_row['id']);
@@ -108,7 +113,7 @@ if($stm){
     //Пишем итоговую статистику 
     $sheet->setCellValueByColumnAndRow(1, $s_column+2, 'НАЧАЛО СТАТИСТИКИ');
     $sheet->setCellValueByColumnAndRow(1, $s_column+3, "Кол-во вопросов: $question_count");
-    
+
     $writer = new Xlsx($spreadSheet);
     header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     header('Content-Disposition: attachment;filename="demo.xlsx"');
