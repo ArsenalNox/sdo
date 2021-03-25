16cf5f1a1aa3f0b05ca6bdacbb2bc4f26d811f0c
ArsenalNox <nohtimore@gmail.com>
2021-03-25 18:35:57 +0500

    Вторая таблица с сортом результатов, условное форматирование

--- php/functions/newExport.php
+++ php/functions/newExport.php
@@ -8,9 +8,32 @@ if(isset($user) & isset($password) & isset($server) & isset($database)){
     $user = 'root';
     $password = '';
     $server = 'localhost';
-    $database = 'sdo';
+    $database = 'sdo3';
 
     $dtb = new PDO("mysql:host=$server;dbname=$database;charset=utf8", $user, $password);
+}
+
+function sortStudentsByErrors($students_array){
+    $sorted_array = [];
+    $tmp = '';
+    $sorted = false;
+    $swichOccured = false;
+    while(!$sorted){   
+        $swichOccured = false;
+        for($i = 0; $i<count($students_array)-1; $i++){
+            if($students_array[$i]['wrong_answers'] > $students_array[$i+1]['wrong_answers']){
+                $swichOccured = true;
+                $tmp = $students_array[$i];
+                $students_array[$i] = $students_array[$i+1];
+                $students_array[$i+1] = $tmp;
+            }
+        }
+        if(!$swichOccured){
+            $sorted = true;
+        }
+    }
+    $sorted_array = $students_array;
+    return $sorted_array;
 }
 
 require '../vendor/autoload.php';
@@ -36,34 +59,39 @@ if($stm){
 
     //Заголовки колонн
     $sheet->setCellValueByColumnAndRow(1, $s_column, 'Номер записи');
-    $sheet->setCellValueByColumnAndRow(2, $s_column, 'ФИО');
-    
-    $s_column++;
-
-    //Условное форматирование 
-    $condition_correctness_true = new Conditional();
-    $condition_correctness_true->setConditionType(Conditional::CONDITION_CELLIS)
-                        ->setOperatorType(Conditional::OPERATOR_EQUAL)
-                        ->addCondition('Нет');
-    $condition_correctness_true->getStyle()->getFont()->getColor()->setARGB(Color::COLOR_RED);
+    $sheet->setCellValueByColumnAndRow(2, $s_column, 'ФИО'); $s_column++; //Перейти на строку ниже Условное форматирование 
+    $condition_correctness_true = new Conditional(); 
+    $condition_correctness_true->setConditionType(Conditional::CONDITION_CELLIS) 
+                        ->setOperatorType(Conditional::OPERATOR_EQUAL) 
+                        ->addCondition('0'); 
+    $condition_correctness_true->getStyle()->getFill()->setFillType(Fill::FILL_SOLID)->getEndColor()->setARGB(Color::COLOR_RED);
     
     $condition_correctness_false = new Conditional();
-    $condition_correctness_false->setConditionType(Conditional::CONDITION_CONTAINSTEXT)
-                        ->setOperatorType(Conditional::OPERATOR_CONTAINSTEXT)
-                        ->addCondition('Да');
+    $condition_correctness_false->setConditionType(Conditional::CONDITION_CELLIS)
+                        ->setOperatorType(Conditional::OPERATOR_EQUAL)
+                        ->addCondition('1');
     $condition_correctness_false->getStyle()->getFill()->setFillType(Fill::FILL_SOLID)->getEndColor()->setARGB(Color::COLOR_GREEN);
 
-    $all_conditional_styles[] = $condition_correctness_true;
+    $all_conditional_styles[] = $condition_correctness_true; 
     $all_conditional_styles[] = $condition_correctness_false;
 
     $hideUnnecessaryColumns = true;
+    $enable_hand_formatting = false;
+
+    //Для составления второй отсортированной таблицы 
+    $students_unsorted = [];
+    
 
     while($row = $stm->fetch(PDO::FETCH_ASSOC)){ //Итерация через все результаты 
         //Записывам id/фио
         $sheet->setCellValueByColumnAndRow(1, $s_column, $row['id']); 
         $sheet->setCellValueByColumnAndRow(2, $s_column, $row['student']);
         $sql = "SELECT * FROM tr_".$row['id']."";
-        
+
+        $test_result = [];
+        $test_result['id'] = $row['id'];
+        $test_result['student'] = $row['student'];
+
         $s_row = 3;
 
         $question_count = 0; 
@@ -78,15 +106,24 @@ if($stm){
                 $sheet->setCellValueByColumnAndRow($s_row+1, 1, 'Вариант');
                 $sheet->setCellValueByColumnAndRow($s_row+2, 1, $tr_row['id']);
 
-                //Данные 
+                //Данные в массив для сортировки 
+                $test_result["Задание_$question_count"] = $tr_row['id'];
+                $test_result["Вариант_$question_count"] = $tr_row['Question_var'];
+                $test_result[$question_count] = (($tr_row['Correctness']) ? "1" : "0");
+
+                //1нные 
                 $sheet->setCellValueByColumnAndRow($s_row,   $s_column, $tr_row['id']);
                 $sheet->setCellValueByColumnAndRow($s_row+1, $s_column, $tr_row['Question_var']);
-                $sheet->setCellValueByColumnAndRow($s_row+2, $s_column, (($tr_row['Correctness']) ? "Да" : "Нет"));
-
+                $sheet->setCellValueByColumnAndRow($s_row+2, $s_column, (($tr_row['Correctness']) ? "1" : "0"));
+    
                 if($tr_row['Correctness']){ //Условное форматирование не работает на ДА и НЕТ, но работает на 0 и 1
-                    $sheet->getCellByColumnAndRow($s_row+2, $s_column)->getStyle()->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB(Color::COLOR_GREEN);
+                    if($enable_hand_formatting){
+                        $sheet->getCellByColumnAndRow($s_row+2, $s_column)->getStyle()->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB(Color::COLOR_GREEN);
+                    }
                 } else {
-                    $sheet->getCellByColumnAndRow($s_row+2, $s_column)->getStyle()->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB(Color::COLOR_RED);
+                    if($enable_hand_formatting){
+                        $sheet->getCellByColumnAndRow($s_row+2, $s_column)->getStyle()->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB(Color::COLOR_RED);
+                    }
                     $wrong_answers++;
                 }
                 
@@ -97,23 +134,46 @@ if($stm){
 
                 //Применение условного форматирования 
                 $sheet->getCellByColumnAndRow($s_row+2, $s_column)->getStyle()->setConditionalStyles($all_conditional_styles);
-                $sheet->getCellByColumnAndRow($s_row+2, $s_column)->getStyle()->getFont()->setBold(true);
                
                 $s_row+=3; //Прошли три ячейки
             }
-        } else {
+            
+            //Запись статистики ученика 
+            $sheet->setCellValueByColumnAndRow($s_row, $s_column, "$wrong_answers");
+            $sheet->setCellValueByColumnAndRow($s_row, 1, "Ошибки");
+
+            if($wrong_answers != 0){
+                $percent = round($wrong_answers/$question_count*100);
+            } else {
+                $percent = 0;
+            }
+            $sheet->setCellValueByColumnAndRow($s_row+1, $s_column, ' '.$percent.'%');
+
+            $test_result['wrong_answers'] = $wrong_answers;
+            $test_result['percent'] = ' '.$percent.'%';
+            $students_unsorted[] = $test_result;
+
+            $wrong_answers = 0;
+        } else { 
             $sheet->setCellValueByColumnAndRow($s_row, $s_column, "Не удалось загрузить результат, таблица результатов отсутсвует");
         }
-        $sheet->getCellByColumnAndRow($s_row+1, 1, 'Ошибки');
-        $sheet->getCellByColumnAndRow($s_row+1, $s_column, $wrong_answers);
-        $sheet->getCellByColumnAndRow($s_row+2, $s_column, round($question_count/100*$wrong_answers)."%");
-
         $s_column++;
     }
     //Пишем итоговую статистику 
-    $sheet->setCellValueByColumnAndRow(1, $s_column+2, 'НАЧАЛО СТАТИСТИКИ');
+    $sheet->setCellValueByColumnAndRow(1, $s_column+2, 'Начало статистики');
     $sheet->setCellValueByColumnAndRow(1, $s_column+3, "Кол-во вопросов: $question_count");
+    $sheet->setCellValueByColumnAndRow(1, $s_column+4, 'Конец статистики');
+ 
+    $students_sorted = sortStudentsByErrors($students_unsorted);
+    $sheet->fromArray($students_sorted, null, 'A'.($s_column+6));
+    
+    for($i=0; $i<count($students_sorted); $i++){ //Применение стиля для новой таблицы
+        for($j=0; $j<count($students_sorted[0]); $j){
 
+        }
+    }
+
+   
     $writer = new Xlsx($spreadSheet);
     header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     header('Content-Disposition: attachment;filename="demo.xlsx"');
